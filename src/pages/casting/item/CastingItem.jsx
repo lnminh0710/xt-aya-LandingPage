febfd401ef4a00d70a47e6db0c6a519cc4533bdd
DESKTOP-UCII9AG\DELL <lnminh0710@gmail.com>
2022-12-29 16:26:34 +0700

    Casting

--- src/pages/casting/item/CastingItem.jsx
+++ src/pages/casting/item/CastingItem.jsx
@@ -6,24 +6,45 @@ import { get } from 'lodash';
 
 import styles from './CastingItem.module.scss';
 import { parseDate } from 'utils/convert';
+import { useMemo } from 'react';
+const GenderOptions = [
+  {
+    label: 'Male',
+    value: '0',
+  },
+  {
+    label: 'Female',
+    value: '1',
+  },
+  {
+    label: 'Other',
+    value: '2',
+  },
+];
 
 const fields1 = [
   { title: 'Category', field: 'Category' },
   { title: 'Location', field: 'Location' },
-  { title: 'Gender', field: 'Gender' },
 ];
 
 const fields2 = [
   { title: 'Expires on', field: 'ExpiresOn' },
-  { title: 'Start on', field: 'StartOn' },
+  { title: 'Start on', field: 'JobStartOn' },
 ];
 
 const fields3 = [
-  { title: 'Language', field: 'Languages' },
+  { title: 'Language', field: 'Language' },
   { title: 'Ethnic type', field: 'EthnicType' },
 ];
 const CastingItem = ({ data }) => {
   const { t } = useTranslation(['casting']);
+  const gender = useMemo(() => {
+    const value = data?.Gender;
+    if (!value) return [];
+    value = value.split(',');
+
+    return GenderOptions.filter((_opt) => value.indexOf(_opt.value) > -1);
+  }, [data?.Gender]);
   return (
     <Link
       href={data.IdNewsCasting ? `/casting/${data.IdNewsCasting}` : '#'}
@@ -52,16 +73,31 @@ const CastingItem = ({ data }) => {
                   </div>
                 </div>
               ))}
+              <div className={styles['casting-detail']}>
+                <div className={styles['casting-detail__label']}>
+                  {t('Gender')}:
+                </div>
+                <div className={styles['casting-detail__value']}>
+                  {gender.map((_g, i) =>
+                    i != 0 ? ', ' + t(_g.label) : t(_g.label)
+                  )}
+                </div>
+              </div>
             </div>
             <div className={styles['casting-content__row']}>
               <div className={styles['casting-detail']}>
                 <div className={styles['casting-detail__label']}>
                   {t('Age')}:
                 </div>
-                <div className={styles['casting-detail__value']}>
-                  {t('from')}:{get(data, 'AgeFrom', 0)}
-                  {t('to')}:{get(data, 'AgeTo', 0)}
-                </div>
+                {!!get(data, 'AgeFrom') || !!get(data, 'AgeTo') ? (
+                  <div className={styles['casting-detail__value']}>
+                    {`${get(data, 'AgeFrom', 0)} - ${
+                      get(data, 'AgeTo', 0) || ''
+                    }`}
+                  </div>
+                ) : (
+                  <div className={styles['casting-detail__value']}>N/A</div>
+                )}
               </div>
               {fields2.map((_f, i) => (
                 <div className={styles['casting-detail']} key={i}>
