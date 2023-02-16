1afab3db5c1c914281f09b04363d231ca5485178
DESKTOP-UCII9AG\DELL <lnminh0710@gmail.com>
2023-02-16 17:40:39 +0700

    Add robot file

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <meta name='robots' content='all' />
         <link
           href='https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;700&display=swap'
           rel='stylesheet'
