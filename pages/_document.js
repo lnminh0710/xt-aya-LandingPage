d124469da464d527dea7b4d9739c7c5154808ba3
DESKTOP-UCII9AG\DELL <lnminh0710@gmail.com>
2023-02-27 19:43:29 +0700

    F

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,7 @@ export default function Document() {
           name='google-site-verification'
           content='55AW612_8T7j4pFXCaWLfRA6Icue3Gredo7R9WVNl6E'
         />
+        <meta name='robots' content='all' />
         <link
           href='https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap'
           rel='stylesheet'
